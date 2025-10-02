930cf28204f65ce6ef692e34ecd34f75f510489a
SaherNadine <149719057+SaherNadine@users.noreply.github.com>
2025-10-02 22:36:42 +0200

    test ajoutes

--- src/test/java/re/forestier/edu/UnitTests.java
+++ src/test/java/re/forestier/edu/UnitTests.java
@@ -1,15 +1,17 @@
 package re.forestier.edu;
 
-import org.junit.jupiter.api.*;
-import re.forestier.edu.rpg.player;
-import re.forestier.edu.rpg.Affichage;
-import re.forestier.edu.rpg.UpdatePlayer;
+import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import re.forestier.edu.rpg.Affichage;
+import re.forestier.edu.rpg.UpdatePlayer;
+import re.forestier.edu.rpg.player;
 
 public class UnitTests {
 
@@ -110,7 +112,55 @@ public class UnitTests {
       up.majFinDeTour(p);
       assertThat(p.currenthealthpoints, is(p.healthpoints));
       assertThat(p.healthpoints, is(0));
+      p.currenthealthpoints = 1;
+      p.healthpoints = 4;
+      assertThat(p.getAvatarClass(), is("ADVENTURER"));
 
+      player p2 = new player("Nadine", "Grognak le barbare", "DWARF", 100, new ArrayList<>());
+      p2.inventory.add("Holy Elixir");
+      p2.currenthealthpoints = 1;
+      p2.healthpoints = 4;
+      UpdatePlayer up2= new UpdatePlayer();
+      up2.majFinDeTour(p2);
+      assertThat(p2.getAvatarClass(), is("DWARF"));
+
+      player p3 = new player("Aya", "Grognak le barbare", "ARCHER", 100, new ArrayList<>());
+      p3.inventory.add("Holy Elixir");
+      p3.currenthealthpoints = 1;
+      p3.healthpoints = 4;
+      UpdatePlayer up3= new UpdatePlayer();
+      up3.majFinDeTour(p3);
+      assertThat(p3.getAvatarClass(), is("ARCHER"));
+
+      player p4 = new player("Ikram", "Grognak le barbare", "ARCHER", 100, new ArrayList<>());
+      p4.inventory.add("Magic Bow");
+      p4.currenthealthpoints = 1;
+      p4.healthpoints = 4;
+      UpdatePlayer up4= new UpdatePlayer();
+      up4.majFinDeTour(p4);
+      assertThat(p4.getAvatarClass(), is("ARCHER"));
     }  
 
-}
+    @Test
+    @DisplayName("Test DWARF with Holy Elixir")
+    void testDwarfWithHolyElixir() {
+            player p5 = new player("walaa", "Grognak le barbare", "DWARF", 100, new ArrayList<>());
+            p5.currenthealthpoints = 1;
+            p5.healthpoints = 4;
+            UpdatePlayer up5= new UpdatePlayer();
+            up5.majFinDeTour(p5);
+            assertThat(p5.getAvatarClass(), is("DWARF"));
+    
+            player p6 = new player("Assia", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+            p6.currenthealthpoints = 1;
+            p6.healthpoints = 4;
+            UpdatePlayer up6= new UpdatePlayer();
+            up6.majFinDeTour(p6);
+            assertThat(p6.getAvatarClass(), is("ADVENTURER"));
+        
+        player p7 = new player("ATA", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+        p7.currenthealthpoints = 2;
+        p7.healthpoints = 4;
+        UpdatePlayer up7= new UpdatePlayer();
+        up7.majFinDeTour(p7);
+        assertThat(p7.getAvatarClass(), is("ADVENTURER")); }}
