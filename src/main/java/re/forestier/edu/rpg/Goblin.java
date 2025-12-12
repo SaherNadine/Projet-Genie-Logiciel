e4e66d421cd016707867190ea19e412e34c122c8
SaherNadine <sahermylene@gmail.com>
2025-12-13 00:56:36 +0100

    feat: implémentation de la classe Gobelin et refactoring des classes existantes

---
+++ src/main/java/re/forestier/edu/rpg/Goblin.java
@@ -0,0 +1,47 @@
+package re.forestier.edu.rpg;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Goblin extends Player {
+    
+    public Goblin(String playerName, String avatar_name, int money, ArrayList<String> inventory) {
+        super(playerName, avatar_name,"GOBLIN", money, inventory);
+    }
+
+    @Override
+    public String getAvatarClass() {
+        return "GOBLIN";
+    }
+
+    @Override
+    protected void initializeAbilities() {
+        abilities.put("INT", 2);
+        abilities.put("ATK", 2);
+        abilities.put("ALC", 1);
+    }
+
+    @Override
+    protected HashMap<String, Integer> getAbilitiesForLevel(int level) {
+        HashMap<String, Integer> levelAbilities = new HashMap<>();
+        
+        switch (level) {
+            case 2:
+                levelAbilities.put("ATK", 3);
+                levelAbilities.put("ALC", 4);
+                break;
+            case 3:
+                levelAbilities.put("VIS", 1);
+                break;
+            case 4:
+                levelAbilities.put("DEF", 1);
+                break;
+            case 5:
+                levelAbilities.put("DEF", 2);
+                levelAbilities.put("ATK", 4);
+                break;
+        }
+        
+        return levelAbilities;
+    }
+}
