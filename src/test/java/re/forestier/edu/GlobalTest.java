01bb7a00e81f38ce7fe440cd64cca5f1c5686917
SaherNadine <sahermylene@gmail.com>
2025-12-15 12:27:00 +0100

    ajout de l'affichage

--- src/test/java/re/forestier/edu/GlobalTest.java
+++ src/test/java/re/forestier/edu/GlobalTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import re.forestier.edu.rpg.Adventurer;
 import re.forestier.edu.rpg.Affichage;
+import re.forestier.edu.rpg.Item;
 import re.forestier.edu.rpg.Player;
 
 public class GlobalTest {
@@ -14,9 +15,33 @@ public class GlobalTest {
     @Test
     void testAffichageBase() {
         Player player = new Adventurer("Florian", "Gnognak le Barbare", 200, new ArrayList<>());
-        player.addXp(20);
+        player.addXp(20);  
         player.inventory = new ArrayList<>();
 
         verify(Affichage.afficherJoueur(player));
+    }
+
+    @Test
+    void testToString() {
+        Player player = new Adventurer("Florian", "Gnognak le Barbare", 200, new ArrayList<>());
+        
+        Item sword = new Item("Épée Longue", "Une épée bien aiguisée", 5, 150);
+        Item potion = new Item("Potion de Vie", "Restaure 50 HP", 1, 50);
+        player.addItem(sword);
+        player.addItem(potion);
+
+        verify(player.toString());
+    }
+
+    @Test
+    void testAffichageMarkdown() {
+        Player player = new Adventurer("Florian", "Gnognak le Barbare", 200, new ArrayList<>());
+        
+        Item sword = new Item("Épée Longue", "Une épée bien aiguisée", 5, 150);
+        Item potion = new Item("Potion de Vie", "Restaure 50 HP", 1, 50);
+        player.addItem(sword);
+        player.addItem(potion);
+
+        verify(player.toMarkdown());
     }
 }
